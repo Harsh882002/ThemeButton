b71e037b0bd6fc07b96fa8ada2e4326cf6d0aaaa
Harsh Patle <harshpatle491@gmail.com>
2024-11-12 17:46:03 +0530

    ok

--- DarkLight.jsx
+++ DarkLight.jsx
@@ -1,4 +1,4 @@
-import { Children, useState,use } from "react";
+import { Children, useState,use, useContext } from "react";
 import { createContext } from "react";
 
 export const ThemeContext = createContext();
@@ -18,12 +18,23 @@ export const ThemeProvider =  ({children}) =>{
 
 
 export const DarkLight = () =>{
-    const {theme, handleToggleButton} = use(ThemeContext);
+    const {theme, handleToggleButton} = useContext(ThemeContext);
     return(
-        <div>
-            <h1>Dark Light Mode Website </h1>
-            <p>Hello, My React V19 Fans </p>
-            <button onClick={handleToggleButton}> {theme === "dark"? "Dark Mode" : "Light Mode"} </button>
+        <div className={`p-4 h-lvh flex flex-col justify-center
+         items-center ${
+            theme === "dark"? "bg-gray-800": "bg-white"
+         } ` }>
+            <h1 
+            className={`my-4 text-xl ${theme === "light" ? "text-gray-800": "text-white"}`}
+            >Dark Light Mode Website </h1>
+
+            <p
+            className={`my-2 text-l ${theme === "light" ? "text-gray-900":"text-white"}`}
+            >Hello, My React V19 Fans </p>
+            <button onClick={handleToggleButton}
+            
+            className={`bg-blue-600 hover:bg-blue-400 text-black rounded-md mt-4`}
+            > {theme === "dark"? "Dark Mode" : "Light Mode"} </button>
         </div>
     )
 }
