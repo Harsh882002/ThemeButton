dde080252ce8ad312c0c2d6f73080a2b866cade1
Harsh Patle <harshpatle491@gmail.com>
2024-11-12 17:18:16 +0530

    new

--- DarkLight.jsx
+++ DarkLight.jsx
@@ -1,4 +1,4 @@
-import { Children, useState } from "react";
+import { Children, useState,use } from "react";
 import { createContext } from "react";
 
 export const ThemeContext = createContext();
@@ -7,18 +7,23 @@ export const ThemeProvider =  ({children}) =>{
 
     const [theme, setTheme ]= useState("dark")
 
+    const handleToggleButton = () =>{
+        return setTheme((prevTheme) => (prevTheme === "dark" ? "light": "dark"));
+    };
+
     return(
-        <ThemeContext.Provider value={{ theme }}>{children}</ThemeContext.Provider>
+        <ThemeContext.Provider value={{ theme, handleToggleButton }}>{children}</ThemeContext.Provider>
     );
 };
 
 
 export const DarkLight = () =>{
+    const {theme, handleToggleButton} = use(ThemeContext);
     return(
         <div>
             <h1>Dark Light Mode Website </h1>
             <p>Hello, My React V19 Fans </p>
-            <button>Switch to light Mode </button>
+            <button onClick={handleToggleButton}> {theme === "dark"? "Dark Mode" : "Light Mode"} </button>
         </div>
     )
 }
